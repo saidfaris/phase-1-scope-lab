8829cce09f4c3c448fa89b3e0572e07685a6f1be
sidee <said.issack@student.moringaschool.com>
2023-06-17 14:01:01 +0300

    completed assighnment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob";
+function getCustomerName() {
+  return customerName;
+}
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();}
+
+  const setBestCustomer = "sai";
+  function changeSetBestCustomer() {
+    setBestCustomer = "said";
+  }
+
+  function overwriteBestCustomer() {
+    customerName = customerName;
+  }
+   const leastFavoriteCustomer = "moh";
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "side";
+}
+  
+
